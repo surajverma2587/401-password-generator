de9b72749d139aae7b592185078a2168f2559d1b
surajverma2587 <surajverma2587@gmail.com>
2023-10-30 20:33:39 +0000

    add validation

--- src/components/PasswordGeneratorForm.js
+++ src/components/PasswordGeneratorForm.js
@@ -10,8 +10,35 @@ import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const PasswordGeneratorForm = () => {
+  const initialValues = {
+    passwordLength: "",
+    isLowercase: false,
+    isUppercase: false,
+    isNumeric: false,
+    isSymbol: false,
+  };
+
+  const validationSchema = Yup.object({
+    passwordLength: Yup.number()
+      .required("Please enter a valid number.")
+      .min(8, "Please enter a password length of minimum 8 characters.")
+      .max(100, "Please enter a password length of maximum 100 characters."),
+    isLowercase: false,
+    isUppercase: false,
+    isNumeric: false,
+    isSymbol: false,
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  const formik = useFormik({ initialValues, validationSchema, onSubmit });
+
   return (
     <Box>
       <Container maxWidth="lg">
@@ -21,11 +48,17 @@ export const PasswordGeneratorForm = () => {
             padding: "2rem",
           }}
         >
-          <Stack component="form" spacing={2}>
+          <Stack component="form" spacing={2} onSubmit={formik.handleSubmit}>
             <TextField
               label="Password Length"
               variant="outlined"
               type="number"
+              value={formik.values.passwordLength}
+              name="passwordLength"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={!!formik.errors.passwordLength}
+              helperText={formik.errors.passwordLength}
             />
 
             <Stack spacing={2}>
@@ -41,11 +74,25 @@ export const PasswordGeneratorForm = () => {
                   <Grid item xs={6}>
                     <Stack>
                       <FormControlLabel
-                        control={<Checkbox />}
+                        control={
+                          <Checkbox
+                            checked={formik.values.isLowercase}
+                            name="isLowercase"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                        }
                         label="Lowercase"
                       />
                       <FormControlLabel
-                        control={<Checkbox />}
+                        control={
+                          <Checkbox
+                            checked={formik.values.isUppercase}
+                            name="isUppercase"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                        }
                         label="Uppercase"
                       />
                     </Stack>
@@ -53,11 +100,25 @@ export const PasswordGeneratorForm = () => {
                   <Grid item xs={6}>
                     <Stack>
                       <FormControlLabel
-                        control={<Checkbox />}
+                        control={
+                          <Checkbox
+                            checked={formik.values.isNumeric}
+                            name="isNumeric"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                        }
                         label="Numeric"
                       />
                       <FormControlLabel
-                        control={<Checkbox />}
+                        control={
+                          <Checkbox
+                            checked={formik.values.isSymbol}
+                            name="isSymbol"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                        }
                         label="Symbols"
                       />
                     </Stack>
@@ -66,7 +127,7 @@ export const PasswordGeneratorForm = () => {
               </FormGroup>
             </Stack>
 
-            <Button variant="contained" color="success">
+            <Button variant="contained" color="success" type="submit">
               Generate
             </Button>
           </Stack>
